355e5b0e4d2b1fb560d67202187e27b1f459448c
Nathan Jones <njonesh@gmu.edu>
2012-03-22 13:50:53 -0400

    settings mod

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'probmap',# Or path to database file if using sqlite3.
         'USER': 'probmap',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
