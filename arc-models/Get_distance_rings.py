acce31facfca3b663f56e280f349e7bd36e1fca1
ezcawi <ecawi@gmu.edu>
2014-03-14 11:25:08 -0500

    finished distance, able to convert to pngs
    runs quite fast, using log transform i could turn data into pngs with a couple steps

--- arc-models/Get_distance_rings.py
+++ arc-models/Get_distance_rings.py
@@ -2,7 +2,7 @@ import os
 import sys
 import arcpy
 import shutil
-
+from arcpy.sa import*
 inws = arcpy.GetParameterAsText(0)
 arcpy.env.workspace = inws
 
@@ -30,9 +30,7 @@ ptList = [];
 pointlist = [];
 pt = arcpy.Point()
 ptGeoms = []
-Distances = []
-DistancePlus = []
-perct = ['25%','50%','75%','95%','100%']
+
 
 i = 0;
 j = 0;
@@ -55,204 +53,205 @@ cat_list = ["Abduction", "Aircraft", "Angler_temperate_mountainous", \
 "Mental_retardation_else", "Mountain_biker_dry", "Mountain_biker_else",\
 "Other_Exsport", "Runner", "Skier_alpine", "Skier_nordic_dry", "Skier_nordic_else",\
 "Snowboarder", "Snowmobiler_dry", "Snowmobiler_temperate_flat", "Snowmobiler_else",\
-"Other"]
-def gettif(Subject_Category,EcoReg,Terrain):
+"Substance Abuse","Other"]
+
+def getpng(Subject_Category,EcoReg,Terrain):
     if Subject_Category == "Abduction":
-        fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[0] + ".tif"
+        fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[0] + ".png"
 
     elif Subject_Category == "Aircraft":
-        fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[1] + ".tif"
+        fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[1] + ".png"
 
     elif Subject_Category == "Angler":
         if EcoReg == "Temperate":
             if Terrain == "Mountainous":
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[2] + ".tif"
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[2] + ".png"
             else:
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[3] + ".tif"
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[3] + ".png"
         elif EcoReg == "Dry":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[4] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[4] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[5] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[5] + ".png"
 
     elif Subject_Category == "All Terrain Vehicle":
-        fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[6] + ".tif"
+        fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[6] + ".png"
 
     elif Subject_Category == "Autistic":
         if EcoReg == "Urban":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[7] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[7] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[8] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[8] + ".png"
 
     elif Subject_Category == "Camper":
         if Terrain == "Mountainous":
             if EcoReg == "Dry":
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[9] + ".tif"
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[9] + ".png"
             else:
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[10] + ".tif"
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[10] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[11] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[11] + ".png"
 
     elif Subject_Category == "Child (1-3)":
         if EcoReg == "Dry":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[12] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[12] + ".png"
         elif EcoReg == "Urban":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[13] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[13] + ".png"
         elif EcoReg == "Temperate" and Terrain == "Mountainous":
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[14] + ".tif"
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[14] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[15] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[15] + ".png"
 
     elif Subject_Category == "Child (4-6)":
         if EcoReg == "Dry":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[16] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[16] + ".png"
         elif EcoReg == "Urban":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[17] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[17] + ".png"
         elif EcoReg == "Temperate" and Terrain == "Mountainous":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[18] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[18] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[19] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[19] + ".png"
 
     elif Subject_Category == "Child (7-9)":
         if EcoReg == "Dry":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[20] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[20] + ".png"
         elif EcoReg == "Urban":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[21] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[21] + ".png"
         elif EcoReg == "Temperate" and Terrain == "Mountainous":
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[22] + ".tif"
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[22] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[23] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[23] + ".png"
 
     elif Subject_Category == "Child (10-12)":
         if EcoReg == "Dry":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[24] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[24] + ".png"
         elif EcoReg == "Urban":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[25] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[25] + ".png"
         elif EcoReg == "Temperate" and Terrain == "Mountainous":
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[26] + ".tif"]
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[26] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[27] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[27] + ".png"
 
     elif Subject_Category == "Child (13-15)":
         if EcoReg == "Dry":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[28] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[28] + ".png"
         elif EcoReg == "Temperate" and Terrain == "Mountainous":
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[29] + ".tif"
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[29] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[30] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[30] + ".png"
 
     elif Subject_Category == "Climber":
-        fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[31] + ".tif"
+        fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[31] + ".png"
 
     elif Subject_Category == "Dementia":
         if EcoReg == "Dry" and Terrain == "Mountainous":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[32] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[32] + ".png"
         elif EcoReg == "Dry" and Terrain == "Flat":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[33] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[33] + ".png"
         elif EcoReg == "Urban":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[34] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[34] + ".png"
         elif EcoReg == "Temperate" and Terrain == "Mountainous":
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[35] + ".tif"
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[35] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[36] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[36] + ".png"
 
     elif Subject_Category == "Despondent":
         if EcoReg == "Dry" and Terrain == "Mountainous":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[37] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pnga/" + cat_list[37] + ".png"
         elif EcoReg == "Dry" and Terrain == "Flat":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[38] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[38] + ".png"
         elif EcoReg == "Urban":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[39] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[39] + ".png"
         elif EcoReg == "Temperate" and Terrain == "Mountainous":
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[40] + ".tif"
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[40] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[41] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[41] + ".png"
 
     elif Subject_Category == "Gatherer":
         if EcoReg == "Dry":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[42] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[42] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[43] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[43] + ".png"
 
     elif Subject_Category == "Hiker":
         if EcoReg == "Dry" and Terrain == "Mountainous":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[44] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[44] + ".png"
         elif EcoReg == "Dry" and Terrain == "Flat":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[45] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[45] + ".png"
         elif EcoReg == "Temperate" and Terrain == "Mountainous":
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[46] + ".tif"
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[46] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[47] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[47] + ".png"
 
     elif Subject_Category == "Horseback Rider":
-        fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[48] + ".tif"
+        fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[48] + ".png"
 
     elif Subject_Category == "Hunter":
         if EcoReg == "Dry" and Terrain == "Mountainous":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[49] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[49] + ".png"
         elif EcoReg == "Dry" and Terrain == "Flat":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[50] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[50] + ".png"
         elif EcoReg == "Temperate" and Terrain == "Mountainous":
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[51] + ".tif"
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[51] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[52] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[52] + ".png"
 
     elif Subject_Category == "Mental Illness":
         if EcoReg == "Urban":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[53] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[53] + ".png"
         elif EcoReg == "Temperate" and Terrain == "Mountainous":
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[54] + ".tif"
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[54] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[55] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[55] + ".png"
 
     elif Subject_Category == "Mental Retardation":
         if EcoReg == "Dry":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[56] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[56] + ".png"
         elif EcoReg == "Urban":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[57] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[57] + ".png"
         elif EcoReg == "Temperate" and Terrain == "Mountainous":
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[58] + ".tif"
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[58] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[59] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[59] + ".png"
 
     elif Subject_Category == "Mountain Biker":
         if EcoReg == "Dry":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[60] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[60] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[61] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[61] + ".png"
 
     elif Subject_Category == "Other (Extreme Sport)":
-        fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[62] + ".tif"
+        fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[62] + ".png"
 
     elif Subject_Category == "Runner":
-        fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[63] + ".tif"
+        fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[63] + ".png"
 
     elif Subject_Category == "Skier-Alpine":
-        fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[64] + ".tif"
+        fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[64] + ".png"
 
     elif Subject_Category == "Skier-Nordic":
         if EcoReg == "Dry":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[65] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[65] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[66] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[66] + ".png"
 
     elif Subject_Category == "Snowboarder":
-        fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[67] + ".tif"
+        fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[67] + ".png"
 
     elif Subject_Category == "Snowmobiler":
         if EcoReg == "Dry":
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[68] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[68] + ".png"
         elif EcoReg == "Temperate" and Terrain == "Flat":
-                fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[69] + ".tif"
+                fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[69] + ".png"
         else:
-            fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[70] + ".tif"
+            fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[70] + ".png"
 
     elif Subject_Category == "Substance Abuse":
-        fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[71] + ".tif"
+        fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[71] + ".png"
 
     else:
-        fname = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[72] + ".tif"
+        fname = "C:/Users/Eric Cawi/Documents/SAR/pngs/" + cat_list[72] + ".png"
 
-    return Distances
+    return fname
 
 fctable = 'lostperson'
 
@@ -280,29 +279,34 @@ for value in range(len(stateabbrv)):
         list(nameval)
         for val in nameval:
             if (val >='0' and val <= '9'):
-                keystr = keystr + val
+                keystr = keystr + val     
                    
         keyval = keystr;
         totname = stateabbrv[value] + keyval
         key.append(totname);
         keystr = ''
-
+#transform to integers        
 for i in range(len(cat_list)):
 	raster_name = "C:/Users/Eric Cawi/Documents/SAR/tifs/" + cat_list[i] + ".tif"
-	transform_name = "C:/Users/Eric Cawi/Documents/Sar/tifs/transform" + cat_list[i] + ".tif"
+	transform_name = "C:/Users/Eric Cawi/Documents/SAR/tifs/transform" + cat_list[i] + ".tif"
+	conversion_name = "C:/Users/Eric Cawi/Documents/SAR/16bittifs/" + cat_list[i] + ".tif"
+	png_name = "C:/Users/Eric Cawi/Documents/SAR/pngs"
 	transform_raster = 5*Int(Log2(raster_name))+255
-	transform_raster.save(transform_name)
 	
+	transform_raster.save(transform_name)
+	arcpy.CopyRaster_management(transform_name, conversion_name,"DEFAULTS","","","","","16_BIT_UNSIGNED","NONE")
+	arcpy.RasterToOtherFormat_conversion(conversion_name,png_name, "PNG")
 
 for i in range(len(key)):
         
         sc = subject_category[i]
         ecoreg = ecoregion_domain[i]
         terr = terrain[i]
-        fname = gettif(sc,ecoreg,terr)#gets the appropriate distance tif for each point in the pointlist
-        point_name = "C:\Users\Eric Cawi\Documents\SAR\test_cases\Distance_" + key[i]+".tif" # the proper format for the thingy
-        shutil.copy(fname,point_name) #arcpy copy wasn't working. This hopefully does.
+        fname = getpng(sc,ecoreg,terr)#gets the appropriate distance tif for each point in the pointlist
+        #transform to integers
+        
+        point_name = "C:/Users/Eric Cawi/Documents/SAR/test_cases/Distance_" + key[i]+".png" # the proper format for the thingy
+        
+        shutil.copy(fname,point_name)#this should work
 
 exit;
-
-
