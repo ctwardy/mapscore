39aa22e7922fa77eea49c2c6b79bd2388ce86df1
ctwardy <ctwardy@gmu.edu>
2014-08-08 17:00:55 -0400

    crt tweaks to motion_models
    Committing locally before merging Eric’s changes from github.

--- arc-models/motion_model.py
+++ arc-models/motion_model.py
@@ -1,4 +1,16 @@
-#motion model simulation
+#!/usr/env/python
+
+#TODO: use the new replacement for PIL, 'Pillow'
+#TODO: convert from 8-space indent to 4-space indent
+
+
+'''Motion Model Simulation
+Eric Cawi
+
+Based on notes from a talk by Stephen Anderson.
+Impedance tables possibly based on work by Ferguson, Doherty, & Doke.
+
+'''
 import numpy as np
 #import arcpy as arc #need to convert rasters to np arrays
 #from arcpy.sa import * #spatial analyst for the thing
@@ -8,27 +20,47 @@ from PIL import Image
 from PIL import PngImagePlugin
 from scipy.stats import rv_discrete
 
+#base_dir = 'C:/Users/Eric Cawi/Documents/SAR'
+base_dir = '.'
+NUM_SIMS = 100
+IMAGE_SIZE = (501,501) # pixels
+
 def tag_image(filename, p_out):#for saving and adding images
         im = Image.open(filename)
         meta = PngImagePlugin.PngInfo()
         meta.add_text("prob outside", str(p_out))
         im.save(filename,"png",pnginfo = meta)
 
-def attract(current_r, current_theta, current_cell, sweep, rast, rast_res,dr):
-        #this function will look at the attractiveness of the 10 directions based on current position and looking direction and an input raster
-        #need to use current theta to figure out which cells of slope to access
+def rectify(sweep_angle):
+        '''Rectifies sweep_angle to lie within [0, 2*pi].'''
+        if sweep_angle < 0:
+                return sweep_angle +2*np.pi
+        elif sweep_angle > 2*np.pi: 
+                return sweep_angle -2*np.pi
+        else:
+                return sweep_angle
         
+def attract(current_r, current_theta, current_cell, sweep, rast, rast_res,dr):
+        '''Rate attractiveness for 10 directions based on current position.
+
+        @param r, theta, cell: Current position info.
+        @param sweep: ???
+        @param rast: current input raster; attractiveness???
+        @param rast_res: raster resolution, i.e. pixel width in meters (???)
+        @param dr: radial distance (???)
+        
+        Need to use current theta to figure out which cells of slope to access
+
+        TODO: why is sweep_cell and array instead of just sweep_x and sweep_y?
+
+        @return vector of relative attractiveness, each scaled from 0..1, sum=1.0
+        
+        '''  
         attract_data= np.zeros(10)
         rel_attract = np.zeros(10)
         for i in range(10):
                 sweep_cell = np.zeros(2)
-                sweep_angle = current_theta + sweep[i]
-                #convert negative angles to a 360 degree version and greater than 360 to right range, this should keep the angles between 0 and 360
-                if (sweep_angle<0):
-                        sweep_angle = sweep_angle +2*np.pi
-                if (sweep_angle>2*np.pi): 
-                        sweep_angle = sweep_angle -2*np.pi
-
+                sweep_angle = rectify(current_theta + sweep[i])
                 #this should work for all quadrants with the right positive negative additions
                 dx = dr*np.cos(sweep_angle)
                 dy = dr*np.sin(sweep_angle)#x and y components of sweep angle
@@ -36,18 +68,30 @@ def attract(current_r, current_theta, current_cell, sweep, rast, rast_res,dr):
                 sweep_cell[1] =  np.floor(dx/rast_res)#get the number of cells to move up/down to look
                 attract_data[i] = rast[current_cell[0]+sweep_cell[0]][current_cell[1]+sweep_cell[1]]#gets actual inverses of slopes
         attract_sum = np.sum(attract_data)
-        #to get relative attractiveness of slope take 1nverse of slope so that less change is bigger, than take each one over the sum over the whole thing
+        #to get relative attractiveness of slope take inverse of slope so that less change is bigger, than take each one over the sum over the whole thing
         rel_attract = attract_data/attract_sum
         return rel_attract#returns relative attractiveness scaled from 0 to 1
         
 def avg_speed(current_cell_sl, new_theta,dr, walking_speeds, sl_res):
-        #will figure out average speed/distance, need to clarify with Dr. Twardy
-        #for simplicity, take average of walking speed of current cell and new cell, as long as there aren;t huge spikes this should be somewhat accurate for the average walking speed of the two places
+        '''Calculates average speed/distance.
+        @param current_cell_sl: (y,x);  y is the rows,  is the columns 
+        @param new_theta:
+        @param dr: radial distance (meters)
+        @param walking_speeds:
+        @param: sl_res:
+        
+        For simplicity, take average of walking speed of current cell and new cell.
+        This should work for small stepsizes.
+        
+        TODO: use the path integral along the radial.
+        TODO: is it (x,y) or (y,x)?
+
+        '''
+        oldx,oldy = current_cell_sl
         dx = np.floor(dr*np.cos(new_theta)/sl_res)
         dy = np.floor(dr*np.sin(new_theta)/sl_res)
-        newy = current_cell_sl[0]+dy
-        newx = current_cell_sl[1] + dx
-        first_speed = walking_speeds[current_cell_sl[0]][current_cell_sl[1]]
+        newx, newy = oldx+dx, oldy+dy
+        first_speed = walking_speeds[oldy][oldx]
         last_speed = walking_speeds[newy][newx]
         speed = (first_speed+last_speed)/2
         return speed
@@ -84,8 +128,8 @@ def main():
         end_time = 240 #arbitrary 4 hours
         #establish initial conditions in polar coordinates, using polar coordinates because i think it's easier to deal with the angles
         #r is the radius from the last known point, theta is the angle from "west"/the positive x axis through the lkp
-        r = np.zeros(1000)#simulates 1000 hikers starting at ipp
-        theta = np.random.uniform(0,2*np.pi,1000) #another 1000x1 array of angles, uniformly distributing heading for simulation
+        r = np.zeros(NUM_SIMS)#simulates 1000 hikers starting at ipp
+        theta = np.random.uniform(0,2*np.pi,NUM_SIMS) #another 1000x1 array of angles, uniformly distributing heading for simulation
         stay = .05
         rev = .05#arbitrary values right now, need to discuss
         sweep = [-45 , -35 , -25 , -15 , -5 , 5 , 15 , 25 , 35, 45 , 0 , 180] #0 represents staying put 180 is a change in heading
@@ -100,13 +144,13 @@ def main():
         current_cell_imp = [imp_shape[0]/2 - 1, imp_shape[1]/2 - 1] #-1 is to compensate for the indeces starting at 0, starting at middle of array should represent the ipp
         current_cell_sl = [sl_shape[0]/2 - 1, sl_shape[1]/2 - 1]
         
-        for i in range(1000):
-                print i
+        for i in range(NUM_SIMS):
+                #print i
                 t = 0
                 current_r = r[i]
                 current_theta = theta[i]
                 while t < end_time:
-                        print t
+                        #print t
                         
                         #look in current direction, need to figure out how to do the sweep of slope
                         slope_sweep = attract(current_r, current_theta,current_cell_sl,sweep,sl, sl_res,dr)
@@ -164,19 +208,25 @@ def main():
                 r[i] = current_r
                 theta[i] = current_theta
         #now that we have final positions for 1000 hikers at endtime the goal is to display/plot
-        probs = np.zeros((500,500))#represents 50 meter cells with ipp at center, will get resized
-        prob_outside = 0
-        for i in range(1000):
-                #convert radius and angle to x and y on the grid
-                x = r[i]*np.cos(theta[i])/50
-                y = r[i]*np.sin(theta[i])/50
-                if x>499 or y>499 or x<0 or y<0:#the hiker ran far away out of hte bounding box
-                        prob_outside+=1/1000
-                else:
-                        probs[250+y][250+x]+=1/1000 #adding 1, dividing by total number of counts to get probability, these numbers should have both positive and negative values
+
+        # Vector math. But why /50?
+        x = r * np.cos(theta)/50
+        y = r * np.sin(theta)/50
+        num_inside = sum(np.where(0<=x<=500 and 0<=y<=500),1,0)
+        prob_outside = (1.-num_inside)/NUM_SIMS
+
+        # Create nominal grid of N 50m cells. Will get resized later.
+        # Avoid zeros by putting 1/N observations in each cell.
+        # TODO: let prior be the distance model, with weight of ~1/100 motion model.
+        probs = np.ones((500,500)) / 250000
+        Loop?
+        probs[250+y][250+x]+=1./NUM_SIMS #adding 1, dividing by total number of counts to get probability, these numbers should have both positive and negative values
         #now add a small probability to those squares with 0, or something like that    
         num_0_cells = 0
         prob_0 = 0
+
+        #TODO: replace these loops with faster np.where() vector operations
+        #or better, why not just start everything with a small nonzero bias.  E.g. np.ones()*.00001 or such
         for i in range(500):
                 for j in range(500):
                         if probs[i][j] ==0:
@@ -193,13 +243,13 @@ def main():
         case_name = 'test'
         #example plotting for testing:
         plt.title("Motion Model Test")
-        plt.imshow(probs,cmap = 'cist_gray')
+        plt.imshow(probs,cmap = 'gist_gray')
         plt.colorbar()
-        name = 'C:/Users/Eric Cawi/Documents/SAR/motion_model_test/%s.png' %case_name
+        name = '%s/motion_model_test/%s.png' % (base_dir, case_name)
         plt.imsave(name, probs, cmap = 'gist_gray')
         tag_image(name, prob_outside)
         #now want to resize the image to be 5001 x5001 as required by mapscore
         img = Image.open(name)
-        img = img.resize((5001,5001), Image.BILINEAR)
+        img = img.resize(IMAGE_SIZE, Image.BILINEAR)
         img.save(name)
 main()
