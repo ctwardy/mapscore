0e9dad4ce64f0f7cdf9460432faaf3186f6e5d41
Mackenzie Sweeney <mackenzie.sweeney@gmail.com>
2014-08-12 09:46:30 -0400

    formatting edits

--- manage.py
+++ manage.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
-import imp
-try:
-    imp.find_module('settings') # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
-    sys.exit(1)
-    
-    
+import sys
 
-import settings
+from django.core.management import execute_manager
+
+
+ERR_MSG = "\
+ERROR: Can't find the file 'settings.py' in the directory containing {}.\n\
+\tIt appears you've customized things.\n\
+\tYou'll have to run django-admin.py, passing it your settings module.\n"
+
+try:
+    # settings module should be in current directory
+    import settings
+except ImportError:
+    sys.stderr.write(ERR_MSG.format(__file__))
+    sys.exit(1)
+
 
 if __name__ == "__main__":
     execute_manager(settings)
