f7842f8ae4b95ba06e1a5bc081acfb3dc18ab120
ezcawi <ecawi@gmu.edu>
2015-01-08 14:48:49 -0500

    Create endpts.m

---
+++ senior-design-code/endpts.m
@@ -0,0 +1,11 @@
+function [spt,ept] = endpts(dx_old, dy_old, dx_new, dy_new,m, n)
+    %dx_old, dy_old - which direction is it coming from
+    %dx_new, dy_new - which direction is it going
+    %m, n - dimensions of array
+    
+    %spt if statements - depend on old values
+    
+    
+    
+    %ept if statements - dpend on new values
+end
