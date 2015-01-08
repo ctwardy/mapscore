39189208f444f66b8114c7c8034221e73624c90c
ezcawi <ecawi@gmu.edu>
2015-01-08 14:48:30 -0500

    Create link_paths.m

---
+++ senior-design-code/link_paths.m
@@ -0,0 +1,9 @@
+function [current_path,higher_res_probs] = link_paths(path_part,current_path,m_old, n_old, m_new,n_new,current_res_probs, lower_res_probs)
+    %path_part - array of all the different path parts
+    %m_old, n_old - dimensions of higher resolution array
+    %m_new, n_new - dimensions of newer resolution arrays
+    %current_res_probs - arrays of probabilities in current resolution
+    %higher_res_probs - arrays of probabilities in lower resolution
+    
+    
+end
