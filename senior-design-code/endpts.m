70fba4b94927833d843d45b32c0ab3930752f8e6
ezcawi <ecawi@gmu.edu>
2015-01-12 09:49:58 -0500

    Update endpts.m

--- senior-design-code/endpts.m
+++ senior-design-code/endpts.m
@@ -5,41 +5,41 @@ function [spt,ept] = endpts(dx_old, dy_old, dx_new, dy_new,m, n)
     
     %spt if statements - depend on old values
     if (dx_old == 0) && (dy_old ==0) %starting point of path, arbitrarily start at top
-        spt = [1, floor(n/2)];
+        spt = [floor(n/2),1];
     elseif (dx_old > 0) && (dy_old == 0) %coming from the left
-        spt = [floor(m/2),1];
+        spt = [1, floor(m/2)];
     elseif (dx_old < 0) && (dy_old == 0) %coming from the right
-        spt = [floor(m/2),n];    
+        spt = [n,floor(m/2)];    
     elseif (dx_old > 0) && (dy_old > 0) %coming from the bottom left
-        spt = [m,1];
+        spt = [1,m];
     elseif (dx_old > 0) && (dy_old < 0) %coming from the top left
         spt = [1,1];
     elseif (dx_old < 0) && (dy_old > 0) %coming from the bottom right
-        spt = [m,n];
+        spt = [n,m];
     elseif (dx_old < 0) && (dy_old < 0) %coming from the top right
-        spt = [1,n];
+        spt = [n,1];
     elseif (dx_old == 0) && (dy_old > 0) %coming from the bottom
-        spt = [m,floor(n/2)];
+        spt = [floor(n/2),m];
     elseif (dx_old == 0) && (dy_old < 0) %coming from the top
-        spt = [1,floor(n/2)];
+        spt = [floor(n/2),1];
     end
     %ept if statements - depend on new values
 
     if (dx_new > 0) && (dy_new == 0) %going right
-        ept = [floor(m/2),n];
+        ept = [n,floor(m/2)];
     elseif (dx_new < 0) && (dy_new == 0) %going left
-        ept = [floor(m/2),1];    
+        ept = [1,floor(m/2)];    
     elseif (dx_new > 0) && (dy_new > 0) %going top right
         ept = [1,n];
     elseif (dx_new > 0) && (dy_new < 0) %going bottom left
-        ept = [m,1];
+        ept = [1,m];
     elseif (dx_new < 0) && (dy_new > 0) %going top left
         ept = [1,1];
     elseif (dx_new < 0) && (dy_new < 0) %going bottom right
-        ept = [m,n];
+        ept = [n,m];
     elseif (dx_new == 0) && (dy_new > 0) %going top
-        ept = [1,floor(n/2)];
+        ept = [floor(n/2),1];
     elseif (dx_new == 0) && (dy_new < 0) %going bottom
-        ept = [m,floor(n/2)];
+        ept = [floor(n/2),m];
     end
 end
