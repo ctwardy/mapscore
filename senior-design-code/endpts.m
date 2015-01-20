d9c9b5a9b4acd63632472e553acd5ed109ab0af9
ezcawi <ecawi@gmu.edu>
2015-01-20 17:48:34 -0500

    fixed starting point logic causing issue in link_path

--- senior-design-code/endpts.m
+++ senior-design-code/endpts.m
@@ -4,23 +4,23 @@ function [spt,ept] = endpts(dx_old, dy_old, dx_new, dy_new,m, n)
     %m, n - dimensions of array
     
     %spt if statements - depend on old values
-    if (dx_old == 0) && (dy_old ==0) %starting point of path, arbitrarily start at top
+    if (dx_old == 0) && (dy_old ==0) %starting point of path, arbitrarily start at top middle
         spt = [floor(n/2),1];
     elseif (dx_old > 0) && (dy_old == 0) %coming from the left
         spt = [1, floor(m/2)];
     elseif (dx_old < 0) && (dy_old == 0) %coming from the right
         spt = [n,floor(m/2)];    
-    elseif (dx_old > 0) && (dy_old > 0) %coming from the bottom left
+    elseif (dx_old > 0) && (dy_old < 0) %coming from the bottom left
         spt = [1,m];
-    elseif (dx_old > 0) && (dy_old < 0) %coming from the top left
+    elseif (dx_old > 0) && (dy_old > 0) %coming from the top left
         spt = [1,1];
-    elseif (dx_old < 0) && (dy_old > 0) %coming from the bottom right
+    elseif (dx_old < 0) && (dy_old < 0) %coming from the bottom right
         spt = [n,m];
-    elseif (dx_old < 0) && (dy_old < 0) %coming from the top right
+    elseif (dx_old < 0) && (dy_old > 0) %coming from the top right
         spt = [n,1];
-    elseif (dx_old == 0) && (dy_old > 0) %coming from the bottom
+    elseif (dx_old == 0) && (dy_old < 0) %coming from the bottom
         spt = [floor(n/2),m];
-    elseif (dx_old == 0) && (dy_old < 0) %coming from the top
+    elseif (dx_old == 0) && (dy_old > 0) %coming from the top
         spt = [floor(n/2),1];
     end
     %ept if statements - depend on new values
