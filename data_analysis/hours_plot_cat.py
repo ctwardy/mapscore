884b341a82f5b9f4a6fe07eb1662a7686520c658
ctwardy <ctwardy@sarbayes.org>
2016-01-18 11:51:21 -0500

    Commit data_analysis from summer.
    Omitting data until we have a private repository.  (It’s dbS’ data.)

--- data_analysis/hours_plot_cat.py
+++ data_analysis/hours_plot_cat.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# hours_plot.py
+# hours_plot_cat.py
 
 """
 Generate a plot of total hours vs. survival rate, given an Excel spreadsheet of
-total hours in the first column and subject status in the second column.
+cases, each with the total hours in the first column, the subject status in 
+the second column, and the subject category in the third.
 """
 
 import math
 import sys
-
 import matplotlib.pyplot as plt
 import numpy as np
 import xlrd
+
+from resample import *
 
 
 CMD_LINE_HELP = '''Usage: python3 {} [options] [filename]
@@ -26,8 +28,8 @@ Options:
 
 1. If both -B and -E are included, -E will override -B.
 2. If -C [categories] is present, it must be the last option.
-3. Categories are separated by spaces.
-4. A "*" will include all categories, which is the same as not using -C.
+   a. Categories are separated by spaces.
+   b. To include all categories, omit -C. 
 
 '''
 
@@ -44,40 +46,15 @@ def binify(hours, bin_scheme):
     raise ValueError("Unhandled value in binify(): {}".format(hours))
 
 
-def bootstrap_resample(X, n=None):
-    """ Bootstrap resample an array_like.
-
-    Used for example, to estimate the variance for a single list of binary outcomes.
-
-    Parameters
-    ----------
-    X : array_like
-      data to resample
-    n : int, optional
-      length of resampled array, equal to len(X) if n==None
-    Results
-    -------
-    returns X_resamples
-
-    From http://nbviewer.ipython.org/gist/aflaxman/6871948
-    """
-    if n == None:
-        n = len(X)
-
-    resample_i = np.floor(np.random.rand(n) * len(X)).astype(int)
-    X_resample = X[resample_i]
-    return X_resample
-
-
 def percent_survival(lst):
+    """Return the percentage of non-DOA entries in the Status list."""
     return (1.0 - 1.0 * lst.count('DOA') / len(lst)) * 100
 
 
 def is_doa(x):
-    if x == 'DOA':
-        return 1
-    return 0
+    return x == 'DOA'
 
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 if len(sys.argv) < 2 or '--help' in sys.argv or '-H' in sys.argv:
     print(CMD_LINE_HELP.format(sys.argv[0]))
@@ -90,86 +67,123 @@ else:
     status_slice = sheet.col_values(1)
     category_slice = [category.upper() for category in sheet.col_values(2)]
     assert len(days_slice) == len(status_slice) == len(category_slice)
-    print('Number of cases: {}'.format(len(days_slice)))
+    print('Number of cases in file : {:5d}'.format(len(days_slice)))
 
-mapping = dict()    # Key: (lowerbound inclusive, upperbound), value: subject status list
-bin_scheme = {
-    # Key: (lowerbound inclusive, upperbound), value: increment size
-    (0, 12): 1,
-    (12, 24): 3,
-    (24, 48): 6,
-    (48, 96): 12,
-    (96, float('inf')): 48
-}
+SCATTER = True
+TRENDLINE = False
+BOXPLOTS = False
+ERRORBARS = False
+CATEGORIES = set(category_slice)
+cat_str = 'All'
 
-selected_categories = set(category_slice)
+if '-s' in sys.argv:
+    SCATTER = False
+if '-T' in sys.argv:
+    TRENDLINE = True
+if '-B' in sys.argv:
+    BOXPLOTS = True
+if '-E' in sys.argv:
+    ERRORBARS = True
 if '-C' in sys.argv:
     start = sys.argv.index('-C')+1
-    if sys.argv[start] != '*':
-        selected_categories = [arg.upper() for arg in sys.argv[start:-1]]    
+    CATEGORIES = [arg.upper() for arg in sys.argv[start:-1]]    
+    cat_str = ','.join(sorted(CATEGORIES))
 
-hours_slice = np.array([binify(days * 24, bin_scheme) for days in days_slice])
-assert len(hours_slice) == len(status_slice) == len(category_slice)
-for hours, status, category in zip(hours_slice, status_slice, category_slice):
-    if status != '': # Ignore cases without a known subject status
-        if category in selected_categories:
-            try:
-                mapping[hours].append(status)
-            except KeyError:
-                mapping[hours] = [status]
+# Define a bin scheme
+# Default increments: [ 6, 12, 24, 48, 256]
+# Increments for All: [ 3,  4,  6, 24,  96]
+# Increments for Des: [ 6, 12, 24, 48, 128]
+# Pfau Bins         : [24, 48, 72, 96, INF]
+# Pfau Increments   : [24, 24, 24, 24, 256]
+INF = float('inf')
+bin_scheme = {
+    # Key: (lowerbound inclusive, upperbound), value: increment size
+    ( 0, 24):  24,
+    (24, 48): 24,
+    (48, 72): 24,
+    (72, 96): 24,
+    (96, INF): 256
+}
 
-x, y, N = list(), list(), list()
-for hours in sorted(mapping.keys()):
-    survival_rate = percent_survival(mapping[hours])
-    num_bins = len(mapping[hours])
-    print('Survival rate at {} hours = {:.1f}% (N={})'.format(hours, survival_rate, num_bins))
-    x.append(hours)
-    y.append(survival_rate)
-    N.append(num_bins)
+# Create hours and binified hours columns, and zip everything into 'data'
+hours_slice = np.array(days_slice)*24
+binhours_slice = np.array([binify(days * 24, bin_scheme) for days in days_slice])
+assert len(binhours_slice) == len(hours_slice) == len(status_slice) == len(category_slice)
+data = zip(binhours_slice, hours_slice, status_slice, category_slice)
 
-if '-T' in sys.argv:
-    z = np.polyfit(x, y, 1, w=N)
-    p = np.poly1d(z)
-    plt.plot(x, p(x), 'b-', alpha=0.7)
-    trend_str = 'Trendline: y = {:.3f}x + {:.3f}'.format(*z)
+# Populate dictionaries for status and hours keyed on binhours
+statuses = {}     # Key: binhours, value: subject status list
+real_hours = {}   # Key: binhours, value: real hours list
+for binhours, hours, status, category in data:
+    if status != '' and category in CATEGORIES:
+        try:
+            statuses[binhours].append(status)
+        except KeyError:
+            statuses[binhours] = [status]
+        try:
+            real_hours[binhours].append(hours)
+        except KeyError:
+            real_hours[binhours] = [hours]
+print('Number of cases selected: {:5d}'.format(len(statuses)))
+
+# Create a datapoint <x,y,n,s> for each binhours entry.
+Xs, Ys, Ns, Ss = [], [], [], []
+for binhours in sorted(statuses.keys()):
+    outcomes = statuses[binhours]
+    survival_rate = percent_survival(outcomes)
+    num_cases = len(outcomes)
+    times = real_hours[binhours]
+    meantime = np.mean(times)
+    DOAs = np.array([is_doa(x) for x in outcomes])
+    surv_rates = [1-np.mean(bootstrap_resample(DOAs)) for i in range(20)]
+    stdev = np.std(surv_rates)*100
+    print('Survival rate at {} hours = {:.1f}% (N={}, sigma={:.1f}%, <t>={:.0f})'.format(
+        binhours, survival_rate, num_cases, stdev, meantime))
+    Xs.append(meantime)
+    Ys.append(survival_rate)
+    Ns.append(num_cases)
+    Ss.append(stdev)
+Xs = np.array(Xs)
+Ys = np.array(Ys)
+Ns = np.array(Ns)
+Ss = np.array(Ss)
+
+if TRENDLINE:
+    Zs = np.polyfit(Xs, Ys, 1, w=Ns)
+    P = np.poly1d(Zs)
+    plt.plot(Xs, P(Xs), 'b-', alpha=0.7)
+    trend_str = 'Trendline: y = {:.3f}x + {:.3f}'.format(*Zs)
     print(trend_str)
     plt.text(.02, .02, trend_str, ha='left', va='bottom',
              transform=plt.gca().transAxes, fontsize=10)
 
-x_pts, y_pts, N_pts = list(), list(), list()
-
-data = list() # For boxplots, if necessary
-for lowerbound, upperbound in bin_scheme:
-    selected_hours = list(hours for hours in x
-                        if lowerbound <= hours < upperbound)
-    survival_rates = [y[x.index(hours)] for hours in selected_hours]
-    x_pts.append(np.average(selected_hours))
-    y_pts.append(np.average(survival_rates))
-    N_pts.append(sum(N[x.index(hours)] for hours in selected_hours))
-    if '-E' in sys.argv:
-        stdev_y = np.std(survival_rates)
-        plt.errorbar(x_pts[-1], y_pts[-1], 1.96 * stdev_y, ecolor='b', alpha=0.2)
-    elif '-B' in sys.argv:
-        data.append(survival_rates)
-
-if '-B' in sys.argv and '-E' not in sys.argv:
-    plt.boxplot(data, positions=x_pts, widths=[10] * len(x_pts))
-
-cat_str = 'All'
-cat_list = sorted(selected_categories)
-if '-C' in sys.argv and '*' not in sys.argv:
-    cat_str = ','.join(cat_list)
+if BOXPLOTS and not ERRORBARS:
+    # TODO: Revisit this -- seems a bit dodgy.   -crt
+    print "***** Check the boxplot code *****"
+    x_pts, y_pts, N_pts = list(), list(), list()
+    box_data = list() # For boxplots, if necessary
+    for lowerbound, upperbound in bin_scheme:
+        selected_hours = list(hours for hours in x
+                            if lowerbound <= hours < upperbound)
+        survival_rates = [y[x.index(hours)] for hours in selected_hours]
+        x_pts.append(np.average(selected_hours))
+        y_pts.append(np.average(survival_rates))
+        N_pts.append(sum(N[x.index(hours)] for hours in selected_hours))
+        box_data.append(survival_rates)
+    plt.boxplot(box_data, positions=x_pts, widths=[10] * len(x_pts))
 
 plt.suptitle('Total Hours vs. Survival Rate', fontsize=16)
-plt.title('%s $(N=%d)$' % (cat_str, sum(N)), fontsize=12)
-plt.xlabel('Total Hours (hours)')
+plt.title('%s $(N=%d)$' % (cat_str, sum(Ns)), fontsize=12)
+plt.xlabel('Total Hours')
 plt.ylabel('Survival Rate (percent)')
-plt.xlim((0, 250))
-plt.ylim((0, 100))
-plt.xticks(range(0, 250, 50))
+plt.xticks(range(0, 240, 24))
 plt.yticks(range(0, 101, 20))
-if not '-s' in sys.argv:
-    plt.scatter(x_pts, y_pts, s=N_pts, alpha=.5)
+plt.xlim((-2, 170))
+plt.ylim((0, 100))
+if SCATTER:
+    plt.scatter(Xs, Ys, s=Ns, alpha=.5)
+if ERRORBARS:
+    plt.errorbar(Xs, Ys, 1.96*Ss, ecolor='b', alpha=0.2)
 plt.get_current_fig_manager().resize(1250, 881)
 plt.savefig(cat_str + '.png')
 plt.show()
---
+++ data_analysis/weather.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+# weather.py
+
+"""
+SARBayes weather.py
+Jonathan Lee
+================================================================================
+The purpose of this module is to provide access to historic weather data using 
+the API of the National Climate Data Center (NCDC).
+
+The API provides the following endpoints: 
+  * datasets
+  * datacategories
+  * datatypes
+  * locationcategories
+  * locations
+  * stations
+  * data
+
+Every call to the API yields JSON data, with one key for results and the other 
+for metadata like so: 
+
+{
+    "results": ..., 
+    "metadata": {
+        "resultset": {
+            "limit": ..., 
+            "count": ..., 
+            "offset": ...
+        }
+    }
+}
+
+The maximum number of results that can be requested per call is 1000. The 
+default limit is 25 items.
+================================================================================
+"""
+
+NOTES = """
+Each measurement is associated with a datatype. For example, TMAX is the maximum temperature of the given time range in tenths of a degree Celsius. I have tried to determine which datatypes were needed for filling in ISRID (see the tuple needed_datatypes in function get_conditions), but it is possible that there may be better datatypes that I have missed (there are about 1500 of them, and many of these are outdated and no longer recorded).
+
+Given a date and a location, the module can query for stations that supported the GHCND dataset, were available at given date, and are located within a certain bounding box. It then goes through each station, requests what it knows, and determines which data are relevant and to be returned. Currently, it is not possible to search by datatypes, which slows down data collection considerably (see the note). Also, the size of the bounding box is only a rough estimate. More testing will be needed.
+"""
+
+import urllib.request, urllib.parse
+import geopy
+import json
+from math import pi, sin, cos, asin, degrees, radians
+
+API_URL = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/{}?{}'
+API_TOKEN = 'CAKlHptoFxCtALqpfyjIKtmpfCbQIWse'
+
+R = 6371 # Earth's radius in km
+
+
+def request_data(endpoint, safe=':,', **parameters):
+    """ Build a URL, fetch the data, and return that data in JSON format. """
+    url = API_URL.format(endpoint, 
+        urllib.parse.urlencode(parameters, safe=safe))
+    request = urllib.request.Request(url, headers={'token': API_TOKEN})
+    response = urllib.request.urlopen(request)
+    data = json.loads(response.read().decode('utf-8'))
+    return data
+
+
+def get_bounds(coordinates, d):
+    """ Get the southwest and northeast coordinates of a bounding box.
+
+    @param coordinates = sequence [latitude, longitude] in decimal degrees
+    @param d = width of the box, in km (float)
+    
+        Sources: 
+          * http://janmatuschek.de/LatitudeLongitudeBoundingCoordinates
+          * https://github.com/jfein/PyGeoTools/blob/master/geolocation.py
+    """
+    lat = radians(coordinates.latitude)
+    lon = radians(coordinates.longitude)
+    r = d/R
+    
+    lat_min, lat_max = lat - r, lat + r
+    # Special case: poles
+    if abs(lat_min) > pi/2:
+        lon_min, lon_max = -pi, pi
+        lat_min, lat_max = max(lat_min, -pi/2), min(lat_max, pi/2)
+    else:
+        change_lon = asin(sin(r)/cos(lat))
+        lon_min, lon_max = lon - change_lon, lon + change_lon
+        if lon_min < -pi:
+            lon_min += 2*pi
+        if lon_max > pi:
+            lon_max -= 2*pi
+    
+    return (
+        geopy.Point(degrees(lat_min), degrees(lon_min)), 
+        geopy.Point(degrees(lat_max), degrees(lon_max))
+    )
+
+
+def get_stations(date, coordinates, datasetid='GHCND', d=20):
+    """ Get the data of the weather stations available at the given date and 
+        coordinates within a bounding box with sides of length 2d.
+    """
+    date_as_str = date.date().isoformat()
+    sw, ne = get_bounds(coordinates, d)
+    extent = ','.join(
+        str(n) for n in [sw.latitude, sw.longitude, ne.latitude, ne.longitude])
+    data = request_data(
+        'stations', 
+        datasetid=datasetid, 
+        startdate=date_as_str, 
+        enddate=date_as_str, 
+        extent=extent
+    )
+    for station in data['results']:
+        yield station
+
+
+def get_conditions(date, coordinates):
+    """ Get the general conditions at a given date and location.
+        Returns as a tuple: 
+          * Maximum temperature (tenths of degrees C)
+          * Minimum temperature (tenths of degrees C)
+          * Average daily wind speed (m/s)
+          * Storm total precipitation
+          * Snowfall (mm)
+    """
+    date_as_str = date.date().isoformat()
+    # Order needs to be maintained (no dictionary)
+    needed_datatypes = ['TMAX', 'TMIN', 'AWND', 'NTP', 'SNOW']
+    needed_values = [None] * len(needed_datatypes)
+    
+    # Normally, the data could be all obtained in a few call, but the API's 
+    # instructions to separate the station IDs with ampersands seems to not 
+    # work because they are interpreted as part of the URL's GET parameters.
+    # URL encoding those ampersands also doesn't seem to work.
+    # For now, make individual requests to each station, but sleep in the loop 
+    # so that the server doesn't shut us out for spamming requests.
+    
+    from time import sleep # Temporary hack
+    
+    for station in get_stations(date, coordinates):
+        data = request_data(
+            'data', 
+            datasetid='GHCND', 
+            startdate=date_as_str, 
+            enddate=date_as_str, 
+            stationid=station['id'], 
+            limit=1000
+        )
+        
+        if 'results' in data:
+            for measurement in data['results']:
+                datatype = measurement['datatype'].strip()
+                if datatype in needed_datatypes:
+                    index = needed_datatypes.index(datatype)
+                    if needed_values[index] is None:
+                        needed_values[index] = measurement['value']
+                        print('Updated:', needed_values)
+                        if None not in needed_values:
+                            return needed_values
+        
+        sleep(1) # Temporary hack
+    else:
+        return needed_values # Some of the data were not able to be obtained
+
+
+import datetime
+date = datetime.datetime(2015, 4, 10, 0, 0, 0)
+coordinates = geopy.point.Point(38.845432, -77.320955)
+print(get_conditions(date, coordinates))
